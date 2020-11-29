cbc949b69038655a173036bfd5cd6192d76f398a
Nilave Chakraborty <nilave.chakraborty@ah.nl>
2020-11-29 08:42:31 +0100

    Update jenkins.go

--- src/models/jenkins.go
+++ src/models/jenkins.go
@@ -5,6 +5,8 @@ type CiJenkins struct {
 	Name     string `json:"name" orm:"column(name)"`
 	Domain   string `json:"domain" orm:"column(domain)"`
 	Location string `json:"location" orm:"column(location)"`
+	Status string `json:"location" orm:"column(status)"`
+	Environ string `json:"location" orm:"column(environ)"`	
 	Type     string `json:"type" orm:"column(type)"`
 	Currver  string `json:"currver" orm:"column(currver)"`
 	Url      string `json:"url" orm:"column(url)"`
