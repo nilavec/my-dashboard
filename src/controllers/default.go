b0a3643009a31c1484e8c59c4160824413de4c00
Nilave Chakraborty <nilave.chakraborty@ah.nl>
2020-12-01 07:43:18 +0100

    Update default.go

--- src/controllers/default.go
+++ src/controllers/default.go
@@ -16,8 +16,18 @@ func (c *MainController) Get() {
 	o := orm.NewOrm()
 
 	var cijenkins []models.CiJenkins
+	var cijenkins_offline []models.CiJenkins
+	var cijenkins_update []models.CiJenkins
+	var cijenkins_security []models.CiJenkins
 	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url")
+	o.QueryTable("ci_jenkins").Filter("status", "Offline").All(&cijenkins_offline, "Name", "Domain", "Location", "Type", "Currver", "Url")
+	o.QueryTable("ci_jenkins").Filter("isupdated", "N").All(&cijenkins_update, "Name", "Domain", "Location", "Type", "Currver", "Url")
+	o.QueryTable("ci_jenkins").Filter("issecure", "N").All(&cijenkins_security, "Name", "Domain", "Location", "Type", "Currver", "Url")	
 
 	c.Data["s"] = cijenkins
+	c.Data["total"] = len(cijenkins)
+	c.Data["offline"] = len(cijenkins_offline)
+	c.Data["update"] = len(cijenkins_update)
+	c.Data["security"] = len(cijenkins_security)	
 	c.TplName = "index.html"
 }
