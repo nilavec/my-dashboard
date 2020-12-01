c4b4dd6e08c01cfa78773d93c40829963ef185d4
Nilave Chakraborty <nilave.chakraborty@ah.nl>
2020-12-01 08:50:29 +0100

    Update default.go

--- src/controllers/default.go
+++ src/controllers/default.go
@@ -31,3 +31,31 @@ func (c *MainController) Get() {
 	c.Data["security"] = len(cijenkins_security)	
 	c.TplName = "index.html"
 }
+func (c *MainController) GetAll() {	
+	o := orm.NewOrm()
+	var cijenkins []models.CiJenkins
+	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url")	
+	c.Data["s"] = cijenkins
+	c.TplName = "all.html"
+}
+func (c *MainController) GetOffline() {	
+	o := orm.NewOrm()
+	var cijenkins_offline []models.CiJenkins
+	o.QueryTable("ci_jenkins").Filter("status", "Offline").All(&cijenkins_offline, "Name", "Domain", "Location", "Type", "Currver", "Url")	
+	c.Data["s"] = cijenkins_offline
+	c.TplName = "offline.html"
+}
+func (c *MainController) GetUpdate() {	
+	o := orm.NewOrm()
+	var cijenkins_update []models.CiJenkins
+	o.QueryTable("ci_jenkins").Filter("isupdated", "N").All(&cijenkins_update, "Name", "Domain", "Location", "Type", "Currver", "Url")
+	c.Data["s"] = cijenkins_update	
+	c.TplName = "update.html"
+}
+func (c *MainController) GetSecurity() {	
+	o := orm.NewOrm()
+	var cijenkins_security []models.CiJenkins
+	o.QueryTable("ci_jenkins").Filter("issecure", "N").All(&cijenkins_security, "Name", "Domain", "Location", "Type", "Currver", "Url")
+	c.Data["s"] = cijenkins_security
+	c.TplName = "security.html"
+}
