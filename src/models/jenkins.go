377425cd37471e82711d6703fec633af0bd2a1ac
Nilave Chakraborty <nilave.chakraborty@ah.nl>
2020-12-01 21:01:03 +0100

    Update jenkins.go

--- src/models/jenkins.go
+++ src/models/jenkins.go
@@ -9,6 +9,7 @@ type CiJenkins struct {
 	Environ string `json:"location" orm:"column(environ)"`	
 	Type     string `json:"type" orm:"column(type)"`
 	Currver  string `json:"currver" orm:"column(currver)"`
+	Lts  string `json:"lts" orm:"column(lts)"`	
 	Url      string `json:"url" orm:"column(url)"`
 	Isupdated      string `json:"isupdated" orm:"column(isupdated)"`
 	Issecure      string `json:"issecure" orm:"column(issecure)"`
