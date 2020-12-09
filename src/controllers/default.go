a4fd3482ea40fcb0906d2ec0d5a9670da6edacc7
Nilave Chakraborty <nilave.chakraborty@ah.nl>
2020-12-09 09:09:27 +0100

    Update default.go

--- src/controllers/default.go
+++ src/controllers/default.go
@@ -30,7 +30,7 @@ func (c *MainController) Get() {
 	var cijenkins_offline []models.CiJenkins
 	var cijenkins_update []models.CiJenkins
 	var cijenkins_security []models.CiJenkins
-	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url")
+	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url", "Cluster", "Nspace", "Descr")
 	o.QueryTable("ci_jenkins").Filter("status", "Offline").All(&cijenkins_offline, "Name", "Domain", "Location", "Type", "Currver", "Url")
 	o.QueryTable("ci_jenkins").Filter("isupdated", "N").All(&cijenkins_update, "Name", "Domain", "Location", "Type", "Currver", "Url")
 	o.QueryTable("ci_jenkins").Filter("issecure", "N").All(&cijenkins_security, "Name", "Domain", "Location", "Type", "Currver", "Url")	
@@ -45,21 +45,21 @@ func (c *MainController) Get() {
 func (c *MainController) GetAll() {	
 	o := orm.NewOrm()
 	var cijenkins []models.CiJenkins
-	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url")	
+	o.QueryTable("ci_jenkins").All(&cijenkins, "Name", "Domain", "Location", "Status","Environ","Type", "Currver", "Url", "Cluster", "Nspace", "Descr")	
 	c.Data["s"] = cijenkins
 	c.TplName = "all.html"
 }
 func (c *MainController) GetOffline() {	
 	o := orm.NewOrm()
 	var cijenkins_offline []models.CiJenkins
-	o.QueryTable("ci_jenkins").Filter("status", "Offline").All(&cijenkins_offline, "Name", "Domain", "Location", "Type", "Currver", "Url")	
+	o.QueryTable("ci_jenkins").Filter("status", "Offline").All(&cijenkins_offline, "Name", "Domain", "Location", "Type", "Currver", "Url", "Cluster", "Nspace", "Descr")	
 	c.Data["s"] = cijenkins_offline
 	c.TplName = "offline.html"
 }
 func (c *MainController) GetUpdate() {	
 	o := orm.NewOrm()
 	var cijenkins_update []models.CiJenkins
-	o.QueryTable("ci_jenkins").Filter("isupdated", "N").All(&cijenkins_update, "Name", "Domain", "Location", "Type", "Currver", "Lts", "Url")
+	o.QueryTable("ci_jenkins").Filter("isupdated", "N").All(&cijenkins_update, "Name", "Domain", "Location", "Type", "Currver", "Lts", "Url", "Cluster", "Nspace", "Descr")
 	c.Data["s"] = cijenkins_update	
 	c.TplName = "update.html"
 }
